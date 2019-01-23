af7526525875eb59805846987c9314fd0077e736
kiikedls <jespinozadelossatnos@gmail.com>
2019-01-22 22:37:56 -0600

    Initial commit

---
+++ app/src/main/java/com/example/rey_m/calculadora/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.rey_m.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Borrar(View view) {
+    }
+
+    @Override
+    public void onClick(View view) {
+        
+    }
+}
